6c272a3dfc8f4bb1e95aae07ee2ed29d9a0fd685
elahesabetinia <elahesabetinias@gmail.com>
2024-11-02 09:09:13 +0330

    Add files via upload

---
+++ all the mirror number between n and m.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // دریافت دو عدد از کاربر
+        Console.Write("Please enter the first number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Please enter the second number: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Mirror numbers between {n} and {m} are:");
+
+        // بررسی هر عدد بین n و m
+        for (int i = n; i <= m; i++)
+        {
+            if (IsMirrorNumber(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+
+    // تابعی برای بررسی آینه‌ای بودن یک عدد
+    static bool IsMirrorNumber(int num)
+    {
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        return original == reversed;
+    }
+}
---
+++ decision tree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DecisionTreeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the decision tree(yes/no)");
+
+            // گره اول: سؤال اولیه
+            Console.WriteLine("are you happy for being alive?");
+            string response = Console.ReadLine().ToLower();
+
+            if (response == "yes")
+            {
+                // گره دوم: گزینه‌های بعدی برای پاسخ "بله"
+                Console.WriteLine("do have any dream or goal in your life");
+                string answer1 = Console.ReadLine().ToLower();
+
+                if (answer1 == "yes")
+                {
+                    Console.WriteLine("Keep trying :) Disney says if you can dream it, you can do it");
+                }
+                else
+                {
+                    Console.WriteLine("How can you be alive and not have a dream? The human purpose of being alive is higher than just being alive.");
+                }
+            }
+            else if (response == "no")
+            {
+                // گره دوم: گزینه‌های بعدی برای پاسخ "خیر"
+                Console.WriteLine("Do you feel worthless?");
+                string xxx = Console.ReadLine().ToLower();
+
+                if (xxx == "yes")
+                {
+                    Console.WriteLine("Don't worry, there is a Japanese proverb that says: When you are a child, you are a bloomer, when you are a teenager, you are a genius...but as an adult, you are just a failure...that's what poverty is. Nature is not something that others can put a price on.");
+                }
+                else
+                {
+                    Console.WriteLine(".I'm glad that at least you believe in yourself :) I agree that life and its people are not interesting things, but being alive is a gift that you have to pay a heavy price to understand its value.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("plese answer just with yes or no.");
+            }
+        }
+    }
+}
